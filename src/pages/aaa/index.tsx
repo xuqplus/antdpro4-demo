7e8697a17875356e1d65dbd9606339b9f092bc00
xuqplus <445172495@qq.com>
2019-07-19 20:21:25 +0800

    使用antd的组件,按钮 Button

--- src/pages/aaa/index.tsx
+++ src/pages/aaa/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import styles from './index.css'
+import { Button } from 'antd'
 
 const data = 'hello, 世界.'
 
@@ -8,6 +9,15 @@ export default function () {
   return (
     <div className={styles.normal}>
       <h1>Page aaa/index data = {data}</h1>
+
+      <Button>这是antd的按钮</Button>
+      <br/>
+      <Button type="default">这是antd的按钮</Button>
+      <Button type="primary">这是antd的按钮</Button>
+      <Button type="ghost">这是antd的按钮</Button>
+      <Button type="dashed">这是antd的按钮</Button>
+      <Button type="danger">这是antd的按钮</Button>
+      <Button type="link">这是antd的按钮</Button>
     </div>
   )
 }
