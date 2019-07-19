2b5784d98679650c12698cea2b6af6f460b12816
xuqplus <445172495@qq.com>
2019-07-19 20:18:12 +0800

    定义常量

--- src/pages/aaa/index.tsx
+++ src/pages/aaa/index.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 import styles from './index.css'
 
+const data = 'hello, 世界.'
+
 export default function () {
   return (
     <div className={styles.normal}>
-      <h1>Page aaa/index</h1>
+      <h1>Page aaa/index data = {data}</h1>
     </div>
   )
 }
