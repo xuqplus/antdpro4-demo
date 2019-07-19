509d3d75ad5b7924cb5e4dceaf6ac7b2f81b49f6
xuqplus <445172495@qq.com>
2019-07-19 21:38:07 +0800

    dispatch, reducers, dva数据流

--- src/models/aaa/a01.js
+++ src/models/aaa/a01.js
@@ -3,5 +3,16 @@ export default {
   state: {
     username: '鲨鱼辣椒',
     xiaodi: '蜻蜓队长',
+    clickCount: 0,
   },
+  reducers: {
+    clickCountPlus: (state) => {
+      let clickCount = state.clickCount += 1
+      return {
+        username: state.username,
+        // xiaodi: state.xiaodi, // 注释掉此项变成null
+        clickCount: clickCount,
+      }
+    }
+  }
 }
--- src/pages/aaa/a01.tsx
+++ src/pages/aaa/a01.tsx
@@ -2,12 +2,23 @@ import React, { Component } from 'react'
 
 import styles from './a01.css'
 import { connect } from 'dva'
+import { Button } from 'antd'
 
 @connect((state) => {
   return {
     username: state['aaa.a01'].username,
-    // xiaodi: state['aaa'].xiaodi,
-    xiaodi: state.aaa.xiaodi,
+    xiaodi: state['aaa.a01'].xiaodi,
+    // xiaodi: state.aaa.xiaodi,
+    clickCount: state['aaa.a01'].clickCount,
+  }
+}, (dispatch) => {
+  return {
+    plus: () => {
+      dispatch({
+        // namespace/方法名
+        type: 'aaa.a01/clickCountPlus',
+      })
+    },
   }
 })
 class A01 extends Component {
@@ -25,6 +36,13 @@ class A01 extends Component {
         </div>
         <div>
           xiaodi = {this.props.xiaodi}
+        </div>
+        <div>
+          点击了多少次 = {this.props.clickCount}
+        </div>
+        <Button type="primary" onClick={this.props.plus}>connect,reducers, 数据流点击</Button>
+        <br/>
+        <div> 这里定义的函数名是 "plus", model里的函数名是 "clickCountPlus"
         </div>
       </div>
     )
