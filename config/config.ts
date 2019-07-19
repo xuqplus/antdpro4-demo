6d131454a6614e71931e584d0d3f46b33699680c
xuqplus <445172495@qq.com>
2019-07-19 20:47:30 +0800

    路由嵌套

--- config/config.ts
+++ config/config.ts
@@ -1,17 +1,17 @@
-import { IConfig, IPlugin } from 'umi-types';
+import { IConfig, IPlugin } from 'umi-types'
 
-import defaultSettings from './defaultSettings';
+import defaultSettings from './defaultSettings'
 // https://umijs.org/config/
-import slash from 'slash2';
-import webpackPlugin from './plugin.config';
+import slash from 'slash2'
+import webpackPlugin from './plugin.config'
 
-const { pwa, primaryColor } = defaultSettings;
+const {pwa, primaryColor} = defaultSettings
 
 // preview.pro.ant.design only do not use in your production ;
 // preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
-const { ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION } = process.env;
+const {ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION} = process.env
 
-const isAntDesignProPreview = ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION === 'site';
+const isAntDesignProPreview = ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION === 'site'
 
 const plugins: IPlugin[] = [
   [
@@ -36,11 +36,11 @@ const plugins: IPlugin[] = [
       },
       pwa: pwa
         ? {
-            workboxPluginMode: 'InjectManifest',
-            workboxOptions: {
-              importWorkboxFrom: 'local',
-            },
-          }
+          workboxPluginMode: 'InjectManifest',
+          workboxOptions: {
+            importWorkboxFrom: 'local',
+          },
+        }
         : false,
       // default close dll, because issue https://github.com/ant-design/ant-design-pro/issues/4665
       // dll features https://webpack.js.org/plugins/dll-plugin/
@@ -59,7 +59,7 @@ const plugins: IPlugin[] = [
       autoAddMenu: true,
     },
   ],
-];
+]
 
 // 针对 preview.pro.ant.design 的 GA 统计代码
 if (isAntDesignProPreview) {
@@ -68,13 +68,13 @@ if (isAntDesignProPreview) {
     {
       code: 'UA-72788897-6',
     },
-  ]);
+  ])
   plugins.push([
     'umi-plugin-pro',
     {
       serverUrl: 'https://ant-design-pro.netlify.com',
     },
-  ]);
+  ])
 }
 
 export default {
@@ -105,7 +105,20 @@ export default {
           path: '/aaa',
           name: 'aaa',
           icon: 'smile',
-          component: './aaa',
+          routes: [
+            {
+              path: '/aaa/index',
+              name: 'aaa',
+              icon: 'smile',
+              component: './aaa',
+            },
+            {
+              path: '/aaa/a01',
+              name: 'aaa - a01',
+              icon: 'smile',
+              component: './aaa/a01',
+            },
+          ],
         },
         {
           component: './404',
@@ -143,21 +156,21 @@ export default {
         context.resourcePath.includes('ant.design.pro.less') ||
         context.resourcePath.includes('global.less')
       ) {
-        return localName;
+        return localName
       }
 
-      const match = context.resourcePath.match(/src(.*)/);
+      const match = context.resourcePath.match(/src(.*)/)
 
       if (match && match[1]) {
-        const antdProPath = match[1].replace('.less', '');
+        const antdProPath = match[1].replace('.less', '')
         const arr = slash(antdProPath)
           .split('/')
           .map((a: string) => a.replace(/([A-Z])/g, '-$1'))
-          .map((a: string) => a.toLowerCase());
-        return `antd-pro${arr.join('-')}-${localName}`.replace(/--/g, '-');
+          .map((a: string) => a.toLowerCase())
+        return `antd-pro${arr.join('-')}-${localName}`.replace(/--/g, '-')
       }
 
-      return localName;
+      return localName
     },
   },
   manifest: {
@@ -173,4 +186,4 @@ export default {
     },
   },
   */
-} as IConfig;
+} as IConfig
---
+++ src/pages/aaa/a01.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './a01.css'
+
+export default function () {
+  return (
+    <div className={styles.normal}>
+      <h1>Page aaa/a01</h1>
+    </div>
+  )
+}
